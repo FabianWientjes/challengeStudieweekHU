4d4c9fe9eb7156d85af6f4d441f841c61d4f3ef2
mjg.gerdes <mjg.gerdes@avans.nl>
2024-08-27 09:21:03 +0200

    dropdown

--- src/dashboard/pages/4_lookalike.py
+++ src/dashboard/pages/4_lookalike.py
@@ -12,6 +12,13 @@ st.title("Look alike companies")
 df=  st.session_state.dataframe
 
 distinct_uuids = df['uuid'].unique()
+# Get the first 15 distinct UUIDs
+first_15_uuids = distinct_uuids[:15]
+
+options_companies = st.selectbox(
+        "Select the reference company",
+        first_15_uuids
+    )
 
 # Define features
 features = ['start_year', 'company_type', 'nace1']  # Replace with actual feature names
@@ -27,7 +34,7 @@ kmeans = KMeans(n_clusters=num_clusters, random_state=42)
 df['cluster'] = kmeans.fit_predict(X_scaled)
 
 # Identify the reference company
-reference_company_id = '9ceb2787-7cf3-41d6-b073-c85a5571210d'  # Replace with actual reference company ID
+reference_company_id = options_companies #'9ceb2787-7cf3-41d6-b073-c85a5571210d'  # Replace with actual reference company ID
 reference_company = df[df['uuid'] == reference_company_id]
 reference_features = reference_company[features]
 
@@ -41,6 +48,7 @@ reference_cluster = kmeans.predict(reference_features_scaled)
 similar_companies = df[df['cluster'] == reference_cluster[0]]
 
 # Output similar companies
-st.text(similar_companies)
+#st.text(similar_companies)
+st.data_editor(similar_companies)
 
 
